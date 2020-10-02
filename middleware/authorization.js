062a464d4112454fb0f4f6205bf22383cb2bf56b
alvin.ramadhani <alvin.ramadhani@dana.id>
2020-10-03 00:42:24 +0700

    sidenav active safety-man dashboard

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -4,19 +4,19 @@ export default function ({ route, redirect }) {
 
   switch(role) {
     case "safety-man":
-      return redirect('/safety-man/dashboard/dashboard')
+      return redirect('/safety-man/dashboard/beranda')
       break;
     case "safety-inspector":
-      return redirect('/safety-inspector/dashboard/dashboard')
+      return redirect('/safety-inspector/dashboard/beranda')
       break;
     case "super-admin":
-      return redirect('/super-admin/dashboard/dashboard')
+      return redirect('/super-admin/dashboard/beranda')
       break;
     case "supervisor":
-      return redirect('/supervisor/dashboard/dashboard')
+      return redirect('/supervisor/dashboard/beranda')
       break;
     case "administrator":
-      return redirect('/administrator/dashboard/dashboard')
+      return redirect('/administrator/dashboard/beranda')
       break;
     default:
       return redirect('/account/login')
