35be4cf8073a138d5f5c30927479f92750cc4416
Alvin <alvin.ertanto@gmail.com>
2020-10-03 17:26:44 +0700

    inspeksi-ulang tambah list group

--- pages/barang/daftarbarang/daftarriwayatinspeksibarang/inspeksi-ulang.vue
+++ pages/barang/daftarbarang/daftarriwayatinspeksibarang/inspeksi-ulang.vue
@@ -19,12 +19,12 @@ export default {
           text: "Daftar Barang",
         },
         {
-          text: "Daftar Riwayat Inspeksi Barang"
+          text: "Daftar Riwayat Inspeksi Barang",
         },
         {
-        text: "Daftar Riwayat",
-          active: true
-        }
+          text: "Daftar Riwayat",
+          active: true,
+        },
       ],
     };
   },
@@ -34,31 +34,57 @@ export default {
 <template>
   <div>
     <PageHeader :title="title" :items="items" />
+    <div class="col-12 mb-3 p-0">
+      <ul class="list-group">
+        <li class="list-group-item list-group-item-light">
+          &gt; Ring Windshock
+        </li>
+        <li class="list-group-item list-group-item-light">
+          &gt; Tiang Windshock
+        </li>
+        <li class="list-group-item list-group-item-light">&gt; Windshock</li>
+        <li class="list-group-item list-group-item-light">
+          &gt; Support Tiang
+        </li>
+      </ul>
+    </div>
 
     <div class="row mb-3">
-        <div class="col-12 mb-3">
-            <label>Keterangan</label>
-            <textarea class="form-control" rows="6" placeholder="Keterangan"></textarea>
-        </div>
-        <div class="col">
-            <label>Tanggal awal berlaku</label>
-            <input type="text" class="form-control"/>
-        </div>
+      <div class="col-12 mb-3">
+        <label>Keterangan</label>
+        <textarea
+          class="form-control"
+          rows="6"
+          placeholder="Keterangan"
+        ></textarea>
+      </div>
+      <div class="col">
+        <label>Tanggal awal berlaku</label>
+        <input type="text" class="form-control" />
+      </div>
 
-        <div class="col">
-            <label>Tanggal akhir berlaku</label>
-            <input type="text" class="form-control"/>
-        </div>
+      <div class="col">
+        <label>Tanggal akhir berlaku</label>
+        <input type="text" class="form-control" />
+      </div>
     </div>
 
     <div class="row">
-        <div class="col-12">
-            <button class="btn btn-success">Simpan</button>
-            <button class="btn btn-danger">Batal</button>
-        </div>
+      <div class="col-12">
+        <button class="btn btn-success">Simpan</button>
+        <button class="btn btn-danger">Batal</button>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+  .list-group {
+  border: 2px solid #d5d3d5;
+  cursor: pointer;
+}
+
+.list-group li:hover {
+  font-weight: bold;
+}
 </style>
