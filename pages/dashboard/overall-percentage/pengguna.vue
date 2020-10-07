162828e3642a3685af05c5c5417b92468af795be
cipauuu <38646181+cipauuu@users.noreply.github.com>
2020-10-07 17:22:12 +0700

    dashboard>overall-percentage>pengguna.vue

---
+++ pages/dashboard/overall-percentage/pengguna.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+  head() {
+    return {
+      title: "Pengguna",
+    };
+  },
+  data() {
+    return {
+      title: "Pengguna",
+      items: [
+        {
+          text: "Dashboard",
+        },
+        {
+          text: "Overall Percentage",
+        },
+        {
+          text: "Pengguna",
+          active: true,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <PageHeader :title="title" :items="items" />
+
+    <div class="row mb-2">
+      <div class="col-12">
+        <button class="btn btn-danger">Kembali</button>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-4">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="mb-0">Admin Balikpapan</h4>
+            <p class="text-muted mb-0">Sun. 2020, Apr 26 18:18:22</p>
+            <img src=""/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
+
+<style>
+</style>
