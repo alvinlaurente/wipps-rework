65c731ee4aabac4f450c8e44ba3814d243bb5ee7
cipauuu <38646181+cipauuu@users.noreply.github.com>
2020-10-07 16:17:25 +0700

    export add iframe

--- pages/dashboard/export.vue
+++ pages/dashboard/export.vue
@@ -2,7 +2,7 @@
 export default {
   head() {
     return {
-      title: "Export"
+      title: "Export",
     };
   },
   data() {
@@ -10,15 +10,23 @@ export default {
       title: "Export",
       items: [
         {
-          text: "Dashboard"
+          text: "Dashboard",
         },
         {
           text: "Export",
-          active: true
-        }
-      ]
+          active: true,
+        },
+      ],
     };
-  }
+  },
+  methods: {
+    showIframe() {
+      document.getElementById("Iframe").style.display = "block";
+    },
+  },
+  mounted: function () {
+    document.getElementById("Iframe").style.display = "none";
+  },
 };
 </script>
 
@@ -27,29 +35,33 @@ export default {
     <PageHeader :title="title" :items="items" />
 
     <!-- Calendar -->
-    <div class="row">
-      <div class="col-4"><span>Dari</span></div>
-      <div class="col-4"><span>Sampai</span></div>
-    </div>
-    <div class="row">
-      <div class="col-4">
+    <div class="row mb-4">
+      <div class="col-md-3">
+        <span>Dari</span>
         <b-form-input type="date" id="input-start-date"></b-form-input>
         <div class="invalid-feedback" id="invalid-start-date">
           <span>Kolom ini tidak boleh kosong.</span>
         </div>
       </div>
-      <div class="col-4">
+      <div class="col-md-3">
+        <span>Sampai</span>
         <b-form-input type="date" id="input-start-date"></b-form-input>
         <div class="invalid-feedback" id="invalid-start-date">
           <span>Kolom ini tidak boleh kosong.</span>
         </div>
       </div>
-      <div class="col-4 text-right">
+      <div class="col-md-6 mt-auto text-right">
         <b-button-group size="md">
-          <b-button variant="danger">Export</b-button>
+          <b-button variant="danger" v-on:click="showIframe">Export</b-button>
           <b-button variant="success">Export</b-button>
           <b-button variant="success">Export Form Data</b-button>
         </b-button-group>
+      </div>
+    </div>
+
+    <div class="row mb-3" id="Iframe">
+      <div class="col-12">
+        <iframe src="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" frameborder="0" style="width: 100%; height: 100vh;"></iframe>
       </div>
     </div>
   </div>
